3b2bee18ff9eba5f455e74e59585fd5581bf5f93
Syed Shayon Saleh <ssaleh@stanford.edu>
2014-01-25 01:36:16 -0800

    Implemented User Login and Sign Up

--- bitter/urls.py
+++ bitter/urls.py
@@ -7,6 +7,10 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bitter.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'bitter_app.views.index'), # root
+    url(r'^login$', 'bitter_app.views.login_view'), # login
+    url(r'^logout$', 'bitter_app.views.logout_view'), # logout
+    url(r'^signup$', 'bitter_app.views.signup'), # signup
 
     url(r'^admin/', include(admin.site.urls)),
 )
